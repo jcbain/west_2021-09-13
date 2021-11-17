e5edba703909abe1a955125199eae406e3de3094
James Bain <james.bain@lighthouselabs.com>
2021-11-17 12:44:59 -0700

    deliver lecture on webpack

---
+++ w10d03_advanced_lecture_webpack/without-webpack/src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+  const links = [
+    {path: '/', name: 'home'},
+    {path: '/buttons', name: 'buttons'}
+  ];
+
+  return React.createElement("div", 
+    {className: 'App'},
+    React.createElement(Header, {links}),
+    React.createElement(CounterComponent, null)
+  )
+};
+
+{/* <div className="App">
+  <Header></Header>
+  <CounterComponent/>
+</div> */}
